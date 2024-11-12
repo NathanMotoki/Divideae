2e1d23054b8075fd7ec46cf7e44675e27b5be2f5
Kauan Eguchi <kauan.eguchi@hotmail.com>
2024-11-11 23:10:12 -0300

    Subindo dependencias spring

---
+++ src/main/java/com/divideae/divideae/DivideaeApplication.java
@@ -0,0 +1,13 @@
+package com.divideae.divideae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivideaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivideaeApplication.class, args);
+	}
+
+}
